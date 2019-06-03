e955f66a7fb902c2a7bbdce13db3c9ef42c4d74b
jacqlynb <jacqlyn.bender@gmail.com>
2019-06-03 14:39:47 -0400

    update mongodb URI for deployment

--- server.js
+++ server.js
@@ -7,14 +7,17 @@ const Chapter = require('./src/models/chapter.js')
 require('./src/db/mongoose')
 
 app.use(express.json())
-// for production 
-// if (process.env.NODE_ENV === 'production') {    
-//   app.use(express.static(path.join(__dirname, 'client/build')));
-// }
-// app.get('*', (req, res) => {    
-//   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));  
-// });
-// end for production
+
+// production stuff
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 
 app.get('/mobydick', (req, res) => {
   Chapter.find({}).sort('idNumerical').then((chapters) => {
@@ -40,17 +43,6 @@ app.post('/mobydick-search', (req, res) => {
     res.send(error)
   });
 })
-
-// production stuff
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-// Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
-
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
 
