55500d52aee418ce52241ed8ad010b4d2604f131
jacqlynb <jacqlyn.bender@gmail.com>
2019-06-03 14:18:07 -0400

    remove yarn.lock

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
+const app = express();
 const port = process.env.PORT || 5000;
 
 const Chapter = require('./src/models/chapter.js')
 require('./src/db/mongoose')
 
 app.use(express.json())
+// for production 
+// if (process.env.NODE_ENV === 'production') {    
+//   app.use(express.static(path.join(__dirname, 'client/build')));
+// }
+// app.get('*', (req, res) => {    
+//   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));  
+// });
+// end for production
 
 app.get('/mobydick', (req, res) => {
   Chapter.find({}).sort('idNumerical').then((chapters) => {
@@ -32,4 +41,23 @@ app.post('/mobydick-search', (req, res) => {
   });
 })
 
+// production stuff
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+
 app.listen(port, () => console.log(`Listening on port ${port}`))
+
+// heroku start
+//"heroku-postbuild": "cd client && npm install && npm install --only=dev --no-shrinkwrap && npm run build",
+
+// dev start
+// "client": "cd client && yarn start",
+//     "server": "nodemon server.js",
+//     "dev": "concurrently --kill-others-on-fail \"yarn server\" \"yarn client\""
